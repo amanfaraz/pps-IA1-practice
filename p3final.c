2900336dac5894d3cdb0a0038a3ead9a2fedd911
amanfaraz2002 <94587617+amanfaraz@users.noreply.github.com>
2022-02-01 06:34:58 +0000

    nthg

---
+++ p3final.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input_n()
+{
+  int a;
+  printf("Enter a number: \n");
+  scanf("%d",&a);
+  return a;
+}
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int n, int sum)
+{
+  for(int i=1;i<n;i++)
+  {
+    printf("%d+",i);
+  }
+  printf("%d is %d \n",n,sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
+}
